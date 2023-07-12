b48b88fe94a320ce09dda3cba4536dfc45deb664
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-12 18:37:04 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
           <Route exact path="/" element={<Home/>} />
           <Route exact path="/freelancers" element={<Freelancers/>} />
           <Route exact path="/clients" element={<Clients/>} />
-          <Route exact path="/projects/:projectId" element={<ProjectDetails/>} />
+          <Route exact path="/projects" element={<ProjectDetails/>} />
           <Route exact path="/profile" element={<Profile/>} />
           <Route exact path="/navbar" element={<Navbar/>} />
           <Route exact path="/freelancerCard" element={<FreelancerCard/>} />
--- src/pages/ProjectDetails.js
+++ src/pages/ProjectDetails.js
@@ -1,35 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import React from 'react';
 
-const ProjectDetails = () => {
-  const { projectId } = useParams();
-  const [project, setProject] = useState(null);
-
-  useEffect(() => {
-    const fetchProject = async () => {
-      try {
-        const response = await axios.get(`/api/projects/${projectId}`);
-        setProject(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchProject();
-  }, [projectId]);
-
-  if (!project) {
-    return <div>Loading...</div>;
-  }
+const ProjectDetails = ({ match }) => {
+  const projectData = {
+    title: 'Project Title',
+    description: 'Project Description',
+    client: 'Client Name',
+    deadline: '2023-07-31',
+  };
 
   return (
     <div>
       <h2>Project Details</h2>
-      <h3>{project.title}</h3>
-      <p>{project.description}</p>
+      <h3>{projectData.title}</h3>
+      <p>{projectData.description}</p>
+      <p>Client: {projectData.client}</p>
+      <p>Deadline: {projectData.deadline}</p>
     </div>
   );
 };
 
 export default ProjectDetails;
+
+
